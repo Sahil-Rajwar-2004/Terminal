e39cc684aed0687260160f937f5f8ba6a9322d32
Sahil Rajwar <sahilrajwar2004@gmail.com>
2023-06-10 20:29:16 +0530

    terminal source file

---
+++ terminal.py
@@ -0,0 +1,53 @@
+import subprocess
+import datetime
+import webbrowser
+import sys
+import os
+
+current_date = datetime.date.today()
+current_day = current_date.strftime("%A")
+print(f"{current_date} {current_day}")
+
+running = True
+
+while running:
+    cwd = os.getcwd()
+    cmd = input(f"[{cwd}]: ")
+    if cmd.startswith("exit"):
+        running = False
+
+    if cmd.startswith("about"):
+        about = r"""creator: Sahil Rajwar
+version: 2023.6.10
+homepage: https://github.com/Sahil-Rajwar-2004/Terminal/
+"""
+        print(about)
+        continue
+
+    if cmd.startswith("homepage"):
+        webbrowser.open("https://github.com/Sahil-Rajwar-2004/Terminal/")
+        continue
+
+    if cmd.startswith("search "):
+        name = cmd[7:].strip()
+        url = "https://www.bing.com/search?q="+name
+        webbrowser.open(url)
+        continue
+
+    if cmd.startswith("cd "):
+        dir_ = cmd[3:].strip()
+        try:
+            os.chdir(dir_)
+        except FileNotFoundError:
+            print(f"directory not found! {dir_}")
+        continue
+
+    try:
+        result = subprocess.run(cmd,shell = True,capture_output = True,text = True)
+        if result.stdout:
+            print(result.stdout)
+        if result.stderr:
+            print(result.stderr,file = sys.stderr)
+    except FileNotFoundError:
+        print(f"invalid command: {cmd}")
+
